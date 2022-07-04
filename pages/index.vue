b256d2feb2f9cbd8c01266a9119bc749937176c6
Victor Marcas <victor11687@gmail.com>
2022-07-04 06:52:06 -0500

    Se ha corregido nombre en component

--- pages/index.vue
+++ pages/index.vue
@@ -7,13 +7,15 @@
             <h1
               class="text-6xl font-bold leading-none text-gray-800 font-montserrat-alt"
             >
-              <span class="block text-2xl text-gray-400">Data que enriquese</span>
+              <span class="block text-2xl text-gray-400"
+                >Data que enriquese</span
+              >
               tu negocio
             </h1>
             <p class="mt-12 text-base font-light leading-loose text-gray-500">
-              Hacemos que la validación de datos sea fácil y accesible para todos,
-              para que puedas asegurar tus transacciones y agregar valor a tu
-              negocio.
+              Hacemos que la validación de datos sea fácil y accesible para
+              todos, para que puedas asegurar tus transacciones y agregar valor
+              a tu negocio.
             </p>
           </div>
         </div>
@@ -25,7 +27,11 @@
     <section class="bg-white py-28">
       <div class="container flex justify-between gap-8 px-12 mx-auto">
         <div class="max-w-md">
-          <h2 class="text-4xl font-bold leading-none text-gray-800 font-montserrat-alt"><span class="block text-lg text-gray-400">Nuestros</span>Productos</h2>
+          <h2
+            class="text-4xl font-bold leading-none text-gray-800 font-montserrat-alt"
+          >
+            <span class="block text-lg text-gray-400">Nuestros</span>Productos
+          </h2>
           <p class="mt-10 text-sm font-light leading-loose text-gray-500">
             Hacemos que la validación de datos sea fácil y accesible para todos,
             para que puedas asegurar tus transacciones y agregar valor a tu
@@ -34,20 +40,44 @@
         </div>
         <div class="grid max-w-2xl grid-cols-2 gap-8">
           <article>
-            <h3 class="text-sm font-bold leading-tight text-gray-800">Identidad personas y empresas</h3>
-            <p class="mt-3 text-xs leading-relaxed text-gray-500">Lorem ipsum dolor sit amet consectetur adipiscing elit varius magna integer, habitant curae tristique euismod fringilla mi ornare conubia ac litora</p>
+            <h3 class="text-sm font-bold leading-tight text-gray-800">
+              Identidad personas y empresas
+            </h3>
+            <p class="mt-3 text-xs leading-relaxed text-gray-500">
+              Lorem ipsum dolor sit amet consectetur adipiscing elit varius
+              magna integer, habitant curae tristique euismod fringilla mi
+              ornare conubia ac litora
+            </p>
           </article>
           <article>
-            <h3 class="text-sm font-bold leading-tight text-gray-800">Identidad personas y empresas</h3>
-            <p class="mt-3 text-xs leading-relaxed text-gray-500">Lorem ipsum dolor sit amet consectetur adipiscing elit varius magna integer, habitant curae tristique euismod fringilla mi ornare conubia ac litora</p>
+            <h3 class="text-sm font-bold leading-tight text-gray-800">
+              Identidad personas y empresas
+            </h3>
+            <p class="mt-3 text-xs leading-relaxed text-gray-500">
+              Lorem ipsum dolor sit amet consectetur adipiscing elit varius
+              magna integer, habitant curae tristique euismod fringilla mi
+              ornare conubia ac litora
+            </p>
           </article>
           <article>
-            <h3 class="text-sm font-bold leading-tight text-gray-800">Identidad personas y empresas</h3>
-            <p class="mt-3 text-xs leading-relaxed text-gray-500">Lorem ipsum dolor sit amet consectetur adipiscing elit varius magna integer, habitant curae tristique euismod fringilla mi ornare conubia ac litora</p>
+            <h3 class="text-sm font-bold leading-tight text-gray-800">
+              Identidad personas y empresas
+            </h3>
+            <p class="mt-3 text-xs leading-relaxed text-gray-500">
+              Lorem ipsum dolor sit amet consectetur adipiscing elit varius
+              magna integer, habitant curae tristique euismod fringilla mi
+              ornare conubia ac litora
+            </p>
           </article>
           <article>
-            <h3 class="text-sm font-bold leading-tight text-gray-800">Identidad personas y empresas</h3>
-            <p class="mt-3 text-xs leading-relaxed text-gray-500">Lorem ipsum dolor sit amet consectetur adipiscing elit varius magna integer, habitant curae tristique euismod fringilla mi ornare conubia ac litora</p>
+            <h3 class="text-sm font-bold leading-tight text-gray-800">
+              Identidad personas y empresas
+            </h3>
+            <p class="mt-3 text-xs leading-relaxed text-gray-500">
+              Lorem ipsum dolor sit amet consectetur adipiscing elit varius
+              magna integer, habitant curae tristique euismod fringilla mi
+              ornare conubia ac litora
+            </p>
           </article>
         </div>
       </div>
@@ -56,7 +86,7 @@
 </template>
 
 <script>
-import SigninBox from '@/components/sections/SignInBox.vue'
+import SigninBox from '@/components/sections/SigninBox.vue'
 export default {
   name: 'IndexPage',
   components: {
--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,58 +1,95 @@
 <template>
   <div class="flex items-center justify-center w-full h-full gap-8">
     <div class="flex-1 max-w-md">
-      <h1 class="text-3xl font-bold leading-tight font-montserrat-alt">Bienvenido,</h1>
-      <p class="mt-2 text-sm leading-tight text-gray-500">Por favor accede a tu cuenta</p>
+      <h1 class="text-3xl font-bold leading-tight font-montserrat-alt">
+        Bienvenido,
+      </h1>
+      <p class="mt-2 text-sm leading-tight text-gray-500">
+        Por favor accede a tu cuenta
+      </p>
       <form @submit.prevent="handleSubmit">
         <fieldset class="py-10 space-y-6">
-          <Input id="email" v-model="form.email" label="Correo eléctronico" type="email"  />
+          <Input
+            id="email"
+            v-model="form.email"
+            label="Correo eléctronico"
+            type="email"
+          />
           <div>
-            <Input id="password" v-model="form.password" label="Contraseña" type="password" />
-            <div class="mt-2 text-right"><NuxtLink to="/forgot-password" class="text-xs leading-tight text-gray-600 hover:text-indigo-500">He olvidado la contraseña</NuxtLink></div>
+            <Input
+              id="password"
+              v-model="form.password"
+              label="Contraseña"
+              type="password"
+            />
+            <div class="mt-2 text-right">
+              <NuxtLink
+                to="/forgot-password"
+                class="text-xs leading-tight text-gray-600 hover:text-indigo-500"
+                >He olvidado la contraseña</NuxtLink
+              >
+            </div>
           </div>
           <div class="flex items-center flex-grow gap-6">
-            <Button type="submit" class="w-full" color="primary">Acceder</Button>
-            <ButtonLink to="/crear-cuenta" border color="secondary" class="w-full" prefetch>Crear cuenta</ButtonLink>
+            <Button type="submit" class="w-full" color="primary"
+              >Acceder</Button
+            >
+            <ButtonLink
+              to="/crear-cuenta"
+              border
+              color="secondary"
+              class="w-full"
+              prefetch
+              >Crear cuenta</ButtonLink
+            >
           </div>
-          <p class="pt-6 text-xs leading-relaxed text-gray-400">Si tienes clientes o varias aplicaciones que necesitan consumir nuestros servicios es.</p>
+          <p class="pt-6 text-xs leading-relaxed text-gray-400">
+            Si tienes clientes o varias aplicaciones que necesitan consumir
+            nuestros servicios es.
+          </p>
         </fieldset>
       </form>
     </div>
     <div class="pl-10">
-      <nuxt-img format="webp" class="w-full max-w-none" width="448" height="448" src="/images/pages/undraw_access_account.png" />
+      <nuxt-img
+        format="webp"
+        class="w-full max-w-none"
+        width="448"
+        height="448"
+        src="/images/pages/undraw_access_account.png"
+      />
     </div>
-</div>
+  </div>
 </template>
 <script>
-  import Input from '@/components/ui/Input.vue'
-  import Button from '@/components/ui/button/Button.vue'
-  import ButtonLink from '@/components/ui/button/ButtonLink.vue'
-  export default {
-    name: 'LoginPage',
-    components: {
-      Input,
-      Button,
-      ButtonLink
-    },
-    layout: 'minimal',
-    data() {
-      return {
-        form: {
-          email:'',
-          password:''
-        }
-      }
-    },
-    head() {
-      return {
-        title: 'Ingresar a mi cuenta'
-      }
-    },
-    methods: {
-      handleSubmit() {
-        console.log(this.form) /* eslint-disable-line no-console */
-      }
+import Input from '@/components/ui/Input.vue'
+import Button from '@/components/ui/button/Button.vue'
+import ButtonLink from '@/components/ui/button/ButtonLink.vue'
+export default {
+  name: 'LoginPage',
+  components: {
+    Input,
+    Button,
+    ButtonLink,
+  },
+  layout: 'minimal',
+  data() {
+    return {
+      form: {
+        email: '',
+        password: '',
+      },
     }
-  }
+  },
+  head() {
+    return {
+      title: 'Ingresar a mi cuenta',
+    }
+  },
+  methods: {
+    handleSubmit() {
+      console.log(this.form) /* eslint-disable-line no-console */
+    },
+  },
+}
 </script>
-
